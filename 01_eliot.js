20105104556f7b56f02e9883e1709520b6cd64cc
Eliot Roberts <eliotroberts@seattleacademy.org>
2016-02-11 12:47:31 -0800

    improve 01 by adding thrid word

--- 01_eliot.js
+++ 01_eliot.js
@@ -1,3 +1,7 @@
-//console.log(process.argv[2])
+//Write a program that takes one argument
+//It should say "hello" and the argument
+//node 01_yourname.js world should output "Hello world"
 
-console.log("hello " + process.argv[2]);
+secondWord = process.argv[2];
+thirdWord = process.argv[3];
+console.log('Hello ' + secondWord + ' ' + thirdWord);
